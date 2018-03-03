11cbfba3e0f9c08a100a92a4fa4f9be41588475c
duncanlindsey <duncan.m.lindsey@gmail.com>
2018-03-03 20:25:55 +0000

    Update

--- CompSciScrape.py
+++ CompSciScrape.py
@@ -3,8 +3,6 @@ import requests, re
 import numpy as np
 import pandas as pd
 
-targetModulesFile = 'TargetModules.csv'
-
 def collectSoup(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
@@ -20,13 +18,46 @@ def parseModuleList(soup, parentURL):
     df = pd.DataFrame(moduleData, columns = ['Code', 'Module', 'Link'])
     return df
 
+def parseModuleDetails(soup):
+    focus_soup = soup.find(class_='syllabus_page')
+    titles = focus_soup.find_all('th')
+    values = focus_soup.find_all('td')
+    #paragraphs = focus_soup.find_all('div', class_=re.compile('csc'))
+    module_details = []
+    for i in range(len(titles)):
+        module_details.append([titles[i].get_text(), values[i].get_text(' ', strip=True)])
+    for header in focus_soup.find_all('div', class_=re.compile('csc-header ')):
+        header_text = header.get_text(strip=True)
+        body_text = []
+        for sibling in header.next_siblings:
+            if sibling.name and sibling.name.startswith('h'):
+                break
+            if sibling.name == 'p':
+                body_text.append(sibling.get_text(' ', strip=True))
+            if sibling.name == 'ul':
+                for bullet in sibling.find_all('li'):
+                    body_text.append('- ' + bullet.get_text(' ', strip=True))
+        module_details.append([header_text, body_text])
+        #header = paragraphs[j].find_all('h1')
+        #body = paragraphs[j].find_all(['p','ul'])
+        #print(header)
+        #print(body)
+        #module_details.append([header.text, body.get_text(' ', strip=True)])
+        #print(headers[j].text)
+        #print(paragraphs[j].text)
+    print(module_details)
+    
+
 def singleSplit(text, delim):
     return (text.partition(delim)[0].strip(), text.partition(delim)[2].strip())
 
-def identifyLinks(library, targets):
-    joined = targets.join(library.set_index('Module'), on = 'Module')
+def joinDataframes(targets, library, onField):
+    joined = targets.join(library.set_index(onField), on = onField)
     return joined
 
 def loadCSV(filename):
     df = pd.read_csv(filename)
     return df
+
+soup = collectSoup('http://www.cs.ucl.ac.uk/current_students/syllabus/compgi/compgi09_applied_machine_learning/')
+parseModuleDetails(soup)
