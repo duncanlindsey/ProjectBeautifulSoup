c1669e934c1f2d0fe133f3ea99a03686f78bcd6a
duncanlindsey <duncan.m.lindsey@gmail.com>
2018-03-04 04:34:48 +0000

    Update

--- CompSciScrape.py
+++ CompSciScrape.py
@@ -20,33 +20,31 @@ def parseModuleList(soup, parentURL):
 
 def parseModuleDetails(soup):
     focus_soup = soup.find(class_='syllabus_page')
+    module_details = {}
+
+    # Scraping the table
     titles = focus_soup.find_all('th')
     values = focus_soup.find_all('td')
-    #paragraphs = focus_soup.find_all('div', class_=re.compile('csc'))
-    module_details = []
     for i in range(len(titles)):
-        module_details.append([titles[i].get_text(), values[i].get_text(' ', strip=True)])
+        module_details[titles[i].get_text()] = values[i].get_text(' ', strip=True)
+    
+    # Scraping the subsequent paragraphs
     for header in focus_soup.find_all('div', class_=re.compile('csc-header ')):
         header_text = header.get_text(strip=True)
         body_text = []
         for sibling in header.next_siblings:
-            if sibling.name and sibling.name.startswith('h'):
+            if sibling.name and sibling.name.startswith('h1'):
                 break
-            if sibling.name == 'p':
-                body_text.append(sibling.get_text(' ', strip=True))
             if sibling.name == 'ul':
                 for bullet in sibling.find_all('li'):
                     body_text.append('- ' + bullet.get_text(' ', strip=True))
-        module_details.append([header_text, body_text])
-        #header = paragraphs[j].find_all('h1')
-        #body = paragraphs[j].find_all(['p','ul'])
-        #print(header)
-        #print(body)
-        #module_details.append([header.text, body.get_text(' ', strip=True)])
-        #print(headers[j].text)
-        #print(paragraphs[j].text)
-    print(module_details)
-    
+            else:
+                body_text.append(sibling.get_text(' ', strip=True))
+        module_details[header_text] = '''
+        '''.join(body_text)
+    module_details['Reading List'] = focus_soup.find(href=re.compile('readinglists.ucl')).get('href')
+    df = pd.DataFrame(module_details, index=[0])
+    return df
 
 def singleSplit(text, delim):
     return (text.partition(delim)[0].strip(), text.partition(delim)[2].strip())
